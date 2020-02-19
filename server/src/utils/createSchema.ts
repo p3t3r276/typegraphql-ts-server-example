8a83fccfe2a5743f31d8e95be921a38ead6657c3
p3t3r276 <p3t3r276@gmail.com>
2020-02-19 22:21:27 +0700

    only use one resolver for simplicity

--- server/src/utils/createSchema.ts
+++ server/src/utils/createSchema.ts
@@ -1,8 +1,10 @@
 import { buildSchema } from "type-graphql";
+import { RegisterResolver } from "../modules/user/Register";
 
 export const createSchema = () =>
   buildSchema({
-    resolvers: [__dirname + "/../modules/*/*.ts"],
+    //resolvers: [__dirname + "/../modules/*/*.ts"],
+    resolvers: [RegisterResolver],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
     }
