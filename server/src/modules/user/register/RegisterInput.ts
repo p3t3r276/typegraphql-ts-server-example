91fb63fc918edf45f42365f4386b6413cfa67025
p3t3r276 <p3t3r276@gmail.com>
2020-02-19 09:25:27 +0700

    remove OkMixin

--- server/src/modules/user/register/RegisterInput.ts
+++ server/src/modules/user/register/RegisterInput.ts
@@ -2,10 +2,9 @@ import { Length, IsEmail } from "class-validator";
 import { InputType, Field } from "type-graphql";
 import { IsEmailAlreadyExist } from "./IsEmailAlreadyExists";
 import { PasswordMixin } from "../../shared/PasswordInput";
-import { OkMixin } from "../../shared/OkMixin";
 
 @InputType()
-export class RegisterInput extends OkMixin(PasswordMixin(class {})) {
+export class RegisterInput extends PasswordMixin(class {}) {
   @Field()
   @Length(1, 30)
   firstName: string;
