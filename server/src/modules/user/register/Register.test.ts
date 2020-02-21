9144155172ddd1b7300f42010bc4ab0f9cb2836a
p3t3r276 <p3t3r276@gmail.com>
2020-02-21 17:14:02 +0700

    Test GraphQL response

--- server/src/modules/user/register/Register.test.ts
+++ server/src/modules/user/register/Register.test.ts
@@ -1,7 +1,9 @@
 import { Connection } from "typeorm";
+import faker from "faker";
 
 import { testConn } from "../../../test-utils/testConn";
 import { gCall } from "../../../test-utils/gCall";
+import { User } from "../../../entity/User";
 
 let conn: Connection;
 beforeAll(async () => {
@@ -27,20 +29,32 @@ const registerMutation = `
 
 describe("Register", () => {
   it("create user", async () => {
-    console.log(
-      JSON.stringify(
-        await gCall({
-          source: registerMutation,
-          variableValues: {
-            data: {
-              firstName: "bob",
-              lastName: "Doe",
-              email: "bob2@bob.com",
-              password: "1234567"
-            }
-          }
-        })
-      )
-    );
+    const user = {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
+    const response = await gCall({
+      source: registerMutation,
+      variableValues: {
+        data: user
+      }
+    });
+
+    expect(response).toMatchObject({
+      data: {
+        register: {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email
+        }
+      }
+    });
+
+    const dbUser = await User.findOne({ where: { email: user.email } });
+    expect(dbUser).toBeDefined();
+    expect(dbUser!.confirmed).toBeFalsy();
+    expect(dbUser!.firstName).toBe(user.firstName);
   });
 });
