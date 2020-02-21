a0dc0e84d36b7bbd3d1ce027bc091f5c42f0eb64
p3t3r276 <p3t3r276@gmail.com>
2020-02-21 20:29:19 +0700

    update list of resolvers

--- server/src/utils/createSchema.ts
+++ server/src/utils/createSchema.ts
@@ -1,10 +1,24 @@
 import { buildSchema } from "type-graphql";
 import { RegisterResolver } from "../modules/user/Register";
+import { ChangePasswordResolver } from "../modules/user/ChangePassword";
+import { ConfirmUserResolver } from "../modules/user/ConfirmUser";
+import { ForgotPasswordResolver } from "../modules/user/ForgotPassword";
+import { LoginResolver } from "../modules/user/Login";
+import { LogoutResolver } from "../modules/user/Logout";
+import { MeResolver } from "../modules/user/Me";
 
 export const createSchema = () =>
   buildSchema({
     //resolvers: [__dirname + "/../modules/*/*.ts"],
-    resolvers: [RegisterResolver],
+    resolvers: [
+      ChangePasswordResolver,
+      ConfirmUserResolver,
+      ForgotPasswordResolver,
+      LoginResolver,
+      LogoutResolver,
+      MeResolver,
+      RegisterResolver
+    ],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
     }
